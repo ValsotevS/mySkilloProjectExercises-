0a904bd480a4ed50448f6e384a5e230e3823014b
Svetoslav Valchev <svetoslav.valchev95@gmail.com>
2023-06-12 17:33:29 +0300

    revenueFromSale

--- Main.java
+++ Main.java
@@ -2,23 +2,33 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        eligibleToWork();
+        revenueFromSale();
     }
 
-    public static void eligibleToWork() {
+    public static void revenueFromSale() {
 
-        System.out.println("Please, Enter you age: ");
+        System.out.println("Enter unit price: ");
 
-        Scanner scanAge = new Scanner(System.in);
+        Scanner scanInput = new Scanner(System.in);
 
-        int age = scanAge.nextInt();
+        float unitPrice = scanInput.nextFloat();
 
-        if (age < 16) {
-            System.out.println("You are NOT eligible to work!");
-        } else if (age > 64){
-            System.out.println("You are NOT eligible to work!");
-        } else {
-            System.out.println("You are ELIGIBLE to work!");
-        }
+        System.out.println("Enter the desired quantity: ");
+
+        float unitQty = scanInput.nextFloat();
+
+        float revenue = unitPrice * unitQty;
+
+            if (unitQty < 100) {
+                System.out.println("The revenue from sale: " + revenue + "$");
+                System.out.println("Discount: 0.00$(0.0%)");
+            } else if (unitQty > 120) {
+                System.out.println("The revenue from sale: " + (revenue - (revenue * 0.20)) + "$");
+                System.out.println("Discount: " + (revenue * 0.20) + "$(20.0%");
+            } else {
+                System.out.println("The revenue from sale: " + (revenue - (revenue * 0.15)) + "$");
+                System.out.println("Discount: " + (revenue * 0.15) + "$(15.0%)");
+            }
+
     }
 }
