f87f02b2ac89fc2107bb49aa019a893106720f4c
Svetoslav <svetoslav.valchev95@gmail.com>
2023-06-13 19:59:53 +0300

    triangleByVortex

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] arrg) {
+    public static void main(String[] args) {
     triangleByVortex();
     }
     public static void triangleByVortex () {
